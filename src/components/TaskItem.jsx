d245ef8e65bd5e6162fa10b6538f91d692e8688c
jorge4684 <js5383651@gmail.com>
2023-04-21 19:03:42 -0600

    task

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const TaskItem = ({ todo }) => {
-  return <div>{todo.task}</div>;
+  const getStyle = () => {
+    return {
+      textDecoration: todo.complete ? "line-through" : "none",
+      margin: "20px",
+      padding: "5px",
+    };
+  };
+  return (
+    <div style={getStyle()}>
+      <input type="checkbox" />
+      {todo.task}
+      <button>X</button>
+    </div>
+  );
 };
 
 export default TaskItem;
